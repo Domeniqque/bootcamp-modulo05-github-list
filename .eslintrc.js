d772854ac5cf7e7937136c0a423f4132a9c6cc93
Domeniqque Dylluar <domeniqque@hotmail.com>
2019-09-10 19:29:10 -0400

    feat: add repository

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,8 @@ module.exports = {
   plugins: ['react', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
-    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
-    'import/prefer-default-export': 'off'
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'import/prefer-default-export': 'off',
+    'react/state-in-constructor': 'off'
   }
 };

bbd07186f0de1e46162f89e65400299cf42b8f75
Domeniqque Dylluar <domeniqque@hotmail.com>
2019-09-13 16:26:19 -0400

    Updates

---
+++ precache-manifest.e47b83df9ebc2f4d1b15bc8b3d841d1c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaebdbe30545c238f7c9a71245f7263a",
+    "url": "/bootcamp-github-repositories/index.html"
+  },
+  {
+    "revision": "482eb8a9ab0faec3c161",
+    "url": "/bootcamp-github-repositories/static/js/2.247a2557.chunk.js"
+  },
+  {
+    "revision": "ea6bfe5beb2b932202b9",
+    "url": "/bootcamp-github-repositories/static/js/main.23d3836d.chunk.js"
+  },
+  {
+    "revision": "8a72a370dfe5e6b64a1f",
+    "url": "/bootcamp-github-repositories/static/js/runtime~main.4d0cb998.js"
+  }
+]);
